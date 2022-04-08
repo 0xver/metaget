eeea48132fc1fcc2a93fa5950094392e3725ceb1
Sam <sam@shlarsen.com>
2022-04-08 03:05:12 -0400

    refactor: Remove state hook

--- components/Structure.js
+++ components/Structure.js
@@ -1,6 +1,6 @@
 import Header from "./Header"
 import Footer from "./Footer"
-import React, { useState } from "react"
+import React from "react"
 
 export default function Structure({ children }) {
     return (
